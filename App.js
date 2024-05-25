825ea90544ae7f60ae30696f3a306d1da1db6546
gitszep <szepdurk@wp.pl>
2024-05-25 23:22:02 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Quiz from './Quiz';
+import './App.css';
+import derekImage from './derek.jpg'; // Importujemy obraz
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <img src={derekImage} alt="Derek" /> 
+      <p>Uratuj mnie !</p>
+
+        <Quiz />
+      </header>
+    </div>
+  );
+}
+
+export default App;
